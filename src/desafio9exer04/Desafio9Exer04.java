a1f12cdaa0c1ad389561209520d7ac239a09cbbd
Davi de Oliveira <odavi076@gmail.com>
2021-05-20 14:08:04 -0300

    Desafio 9 Exercício 4 - 18/05/21.

---
+++ src/desafio9exer04/Desafio9Exer04.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package desafio9exer04;
+
+/**
+ *
+ * @author odavi
+ */
+public class Desafio9Exer04 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int numero;
+        numero = 101;
+        do{
+            System.out.println(numero);
+            numero = numero + 2;
+        }while(200 > numero);
+    }
+    
+}
